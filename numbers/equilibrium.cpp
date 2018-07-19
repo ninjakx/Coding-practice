709da704e9b22a3eeef1ad3e61d3068c527a277b
Kriti <samurai.kittycat@gmail.com>
2018-07-19 21:50:34 +0530

    equib pt of array

---
+++ numbers/equilibrium.cpp
@@ -0,0 +1,40 @@
+//https://practice.geeksforgeeks.org/problems/equilibrium-point
+#include <iostream>
+using namespace std;
+int equi(int arr[],int l,int sum)
+{       
+       if (l==1)
+          return 1;
+       int leftsum=0;
+	   for (int i =0 ;i<l;i++)
+	   {    
+	       sum = sum - arr[i];
+	       if (leftsum == sum)
+	            return i+1;
+	       leftsum+=arr[i];
+	   }
+	   return -1;
+}
+int main() {
+	int t;
+	
+	cin>>t;
+	while(t--)
+	{  int n;
+	   cin>>n;
+       int sum =0,index;
+	   int a[n];
+	    
+	   for(int i=0;i<n;i++)
+	   {
+	       cin>>a[i];
+	       sum = sum + a[i];
+	   }
+	   index = equi(a,n,sum);
+	   cout<<index<<endl;
+	       
+	 
+	   
+	}
+	return 0;
+}
