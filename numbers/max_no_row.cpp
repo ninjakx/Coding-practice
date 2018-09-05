817a822c86c39a075cc85c7309187ba5843b7c1e
Kriti <samurai.kittycat@gmail.com>
2018-09-05 12:11:24 +0530

    Create max_no_row.cpp

---
+++ numbers/max_no_row.cpp
@@ -0,0 +1,50 @@
+//https://practice.geeksforgeeks.org/problems/maximum-no-of-1s-row/0
+#include<unordered_map>
+#include <algorithm>
+#include <map>
+#include <string>
+#include <utility>
+using namespace std;
+struct LessBySecond
+{
+    template <typename Lhs, typename Rhs>
+    bool operator()(const Lhs& lhs, const Rhs& rhs) const
+    {
+        return lhs.second < rhs.second;
+    }
+};
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {   
+        int c,r;
+        cin>>r>>c;
+        map <int, int> hash;
+        hash.clear();
+        int a[40][40];
+	    for(int i=0;i<r;i++)
+	    {
+	        for(int j=0;j<c;j++)
+	        {
+	            cin>>a[i][j];
+	            //cout<<a[i][j]<< " ";
+	            if (a[i][j] == 1)
+	                ++hash[i];
+	                
+	        }
+	        //cout<<endl;
+	    }
+	    
+	 /*for (auto v:hash)
+	 cout<<v.first<<"->"<<v.second<<endl;*/
+    auto x = max_element(hash.begin(), hash.end(),
+    [](const pair<int, int>& p1, const pair<int, int>& p2) {
+        return p1.second < p2.second; });
+        cout<<x->first<<endl;
+        //int max = max_element(hash.begin(), hash.end(), LessBySecond())->first;
+        //cout<<max<<endl;
+    }
+	return 0;
+}
