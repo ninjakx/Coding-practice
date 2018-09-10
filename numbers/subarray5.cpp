873a78d08b7828bdae5f8433ccabf7c63c6d125b
Kriti <samurai.kittycat@gmail.com>
2018-09-10 22:18:04 +0530

    number-subarrays-product-sum-equal

--- numbers/subarray5.cpp
+++ numbers/subarray5.cpp
@@ -6,53 +6,25 @@
 #include<map>
 using namespace std;
 
-long int count_sum_subarray(int a[],int n)
-{
-    long int result = 0;
-    for(int i=0;i<n;i++)
-    {
-        result += a[i]*(n-i)*(i+1);
-    }
-    return result;
-}
-
-int main()
-{   int n = 3;
-    int a[n];
-    for(int i=0;i<n;i++)
-    {
-        cin>>a[i];
-    }
-    cout<<count_sum_subarray(a,n);
-    return 0;
-}
-#include<iostream>
-#include<stdio.h>
-#include<algorithm>
-#include<vector>
-#include<map>
-using namespace std;
-
 int main()
 {
     int n = 4;
     int a[n]={4, 1, 2, 1};
     int i=-1;
     int prod,sum,count = 0;
-    
-    
-    for(int i=0,j=0;i<n;i++)
+    for(int j=0;j<n;j++)
     {
-
-        sum = sum + a[i];
-        prod = prod * a[i];
+        sum =0;
+        prod =1;
+        i=-1;
+    while( ++i<n )
+    {
+        sum = sum + a[i+j];
+        prod = prod * a[i+j];
         if (sum == prod)
             {
                 count++;
-            }
-        
-        
-            
+            }  
     }
     }
     cout<<count;
