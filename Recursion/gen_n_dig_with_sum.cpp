bac3acd4775392a7840934f3c9948613ec3392bb
Kriti <samurai.kittycat@gmail.com>
2021-07-06 20:53:05 +0530

    all n-digit numbers with a given sum of digits

--- Recursion/gen_n_dig_with_sum.cpp
+++ Recursion/gen_n_dig_with_sum.cpp
@@ -48,3 +48,54 @@ int main()
 
 }
 
+
+---------
+    
+    
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string_view>
+
+using namespace std;
+
+class Solution {
+public:
+
+void gen_num(string ans, int n, int ind, int sum){
+    int add = 0;
+    int digit;
+    if (ind==n){
+        if (sum==0){
+            cout<<ans<<endl;
+        }
+        return;
+    }
+
+    if (ind==0){
+        add = 1;
+    }
+    else
+        add = 0;
+
+        for (int i = 0+add; i<=9; i++){
+            digit = i;
+//            ans = ans+to_string(i);
+            gen_num(ans+to_string(i), n, ind+1, sum-digit);
+        }
+}
+
+};
+
+
+
+int main()
+{
+    Solution sol;
+    int n=3;
+    int sum = 14;
+    sol.gen_num("", n, 0, sum);
+
+}
+
+
