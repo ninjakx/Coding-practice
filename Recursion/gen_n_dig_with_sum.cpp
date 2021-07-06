5dd04db167b3f751219467b0024fde01629d8148
Kriti <samurai.kittycat@gmail.com>
2021-07-06 20:44:53 +0530

    all n-digit numbers with a given sum of digits

---
+++ Recursion/gen_n_dig_with_sum.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string_view>
+
+using namespace std;
+
+class Solution {
+public:
+
+void gen_num(vector<char>ans, int n, int ind, int sum){
+    int add = 0;
+    int digit;
+    if (ind==n){
+        if (sum==0){
+            for (auto it: ans){
+                 cout<<it;
+            }
+            cout<<"\n";
+        }
+        return;
+    }
+
+    if (ind==0){
+        add = 1;
+    }
+    else
+        add = 0;
+
+        for (int i = 0+add; i<=9; i++){
+            digit = i;
+            ans[ind] = i+'0';
+            gen_num(ans, n, ind+1, sum-digit);
+        }
+}
+
+};
+
+
+
+int main()
+{
+    Solution sol;
+    int n=3;
+    int sum = 17;
+    vector<char> ch(n, ' ');
+    sol.gen_num(ch, n, 0, sum);
+
+}
+
